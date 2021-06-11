c9cd1d6986a8a406e116806e88b57321e7df3a24
tipuusultan <62797976+tipuusultan@users.noreply.github.com>
2021-06-11 14:48:03 +0000

    Create n.py

---
+++ n.py
@@ -0,0 +1,7 @@
+from plyer import notification
+from plyer import notification
+from time import sleep
+
+while True:
+    notification.notify(title='Take 20 Second Rest' , message='Save your eyes by following 20 20 20 rule')
+    sleep(1200)
